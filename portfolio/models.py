568ac8f7fbd272aacf6e34edd3ff5251bb81c1a2
Buggystepper <linux7506@gmail.com>
2025-06-23 21:08:49 +0300

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 
+# portfolio/models.py
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=100)
     github_link = models.URLField(blank=True, null=True)
+    demo_link = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='projects/', blank=True, null=True)
 
     def __str__(self):
