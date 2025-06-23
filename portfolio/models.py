6bfed50a638a5d32a784cc7f4d495c915271b79a
Buggystepper <linux7506@gmail.com>
2025-06-23 21:41:31 +0300

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 
 # portfolio/models.py
+from django.db import models
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -9,6 +11,10 @@ class Project(models.Model):
     github_link = models.URLField(blank=True, null=True)
     demo_link = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='projects/', blank=True, null=True)
+
+    # ✅ New fields
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
