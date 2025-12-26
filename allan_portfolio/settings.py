a8c31a1ef40b16576bdb9fd3e9688c8c15961707
Buggystepper <linux7506@gmail.com>
2025-12-26 16:00:10 +0300

    Update settings.py

--- allan_portfolio/settings.py
+++ allan_portfolio/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
-from pathlib import Path
 import os
 import dj_database_url
+from pathlib import Path
+from decouple import config
+from django.contrib.messages import constants as messages
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
