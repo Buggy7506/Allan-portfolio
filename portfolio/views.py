aaf82ed56e4583812b5ec6e6543304316ff39811
Buggystepper <linux7506@gmail.com>
2026-01-26 14:49:24 +0300

    Update views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project
 from .forms import ContactForm
+from django.http import HttpResponse
+
+# Ping Page
+def ping(request):
+    return HttpResponse("pong")
 
 def index(request):
     projects = Project.objects.all()
