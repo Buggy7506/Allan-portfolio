587f3fe6ab2ef60199363c97fd522888d5d858c6
Buggystepper <linux7506@gmail.com>
2026-01-26 14:50:29 +0300

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # -------------------------
+    # Ping URL
+    # -------------------------
+    path("ping/", views.ping, name="ping"),
     path('', views.index, name='index'),
     path('like/<int:pk>/', views.like_project, name='like_project'),
 ]
