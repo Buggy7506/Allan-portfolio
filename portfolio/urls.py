aa45a3b4c1d5960880347958711c8f03c2ddb2ad
Buggystepper <linux7506@gmail.com>
2026-02-02 17:12:49 +0300

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,17 @@
+from django.views.generic import RedirectView
 from django.urls import path
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=settings.STATIC_URL + "favicon.ico",
+            permanent=True
+        ),
+    ),
     # -------------------------
     # Ping URL
     # -------------------------
